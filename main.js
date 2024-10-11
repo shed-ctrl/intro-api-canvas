8246ce780362a958ebb0e5adfb6edeaa268ccd56
Shedlie Moussignac <shed-ctrl>
2024-10-10 21:21:19 -0600

    main

--- main.js
+++ main.js
@@ -65,26 +65,57 @@ var ctx = canvas.getContext("2d");
 
   // 6) Trazo Curvas Bézier y cuadráticas
   // Ejemplo de curvas cuadráticas
-    // ctx.beginPath();
-    // ctx.moveTo(75, 25);
-    // ctx.quadraticCurveTo(25, 25, 25, 62.5);
-    // ctx.quadraticCurveTo(25, 100, 50, 100);
-    // ctx.quadraticCurveTo(50, 120, 30, 125);
-    // ctx.quadraticCurveTo(60, 120, 65, 100);
-    // ctx.quadraticCurveTo(125, 100, 125, 62.5);
-    // ctx.quadraticCurveTo(125, 25, 75, 25);
-    // ctx.stroke();
+    /* ctx.beginPath();
+     ctx.moveTo(75, 25);
+     ctx.quadraticCurveTo(25, 25, 25, 62.5);
+     ctx.quadraticCurveTo(25, 100, 50, 100);
+     ctx.quadraticCurveTo(50, 120, 30, 125);
+     ctx.quadraticCurveTo(60, 120, 65, 100);
+     ctx.quadraticCurveTo(125, 100, 125, 62.5);
+     ctx.quadraticCurveTo(125, 25, 75, 25);
+     ctx.stroke()*/
 
    // 7) Trazo Combinaciones de lineas y figuras
-    // Ejemplo de curvas cúbicas
     ctx.beginPath();
-    ctx.moveTo(75, 40);
-    ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
-    ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
-    ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
-    ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
-    ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
-    ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
+    ctx.arc(37, 37, 13, Math.PI / 7, -Math.PI / 7, false);
+    ctx.lineTo(31, 37);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(83, 116);
+    ctx.lineTo(83, 102);
+    ctx.bezierCurveTo(83, 94, 89, 88, 97, 88);
+    ctx.bezierCurveTo(105, 88, 111, 94, 111, 102);
+    ctx.lineTo(111, 116);
+    ctx.lineTo(106.333, 111.333);
+    ctx.lineTo(101.666, 116);
+    ctx.lineTo(97, 111.333);
+    ctx.lineTo(92.333, 116);
+    ctx.lineTo(87.666, 111.333);
+    ctx.lineTo(83, 116);
+    ctx.fill();
+
+    ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.moveTo(91, 96);
+    ctx.bezierCurveTo(88, 96, 87, 99, 87, 101);
+    ctx.bezierCurveTo(87, 103, 88, 106, 91, 106);
+    ctx.bezierCurveTo(94, 106, 95, 103, 95, 101);
+    ctx.bezierCurveTo(95, 99, 94, 96, 91, 96);
+    ctx.moveTo(103, 96);
+    ctx.bezierCurveTo(100, 96, 99, 99, 99, 101);
+    ctx.bezierCurveTo(99, 103, 100, 106, 103, 106);
+    ctx.bezierCurveTo(106, 106, 107, 103, 107, 101);
+    ctx.bezierCurveTo(107, 99, 106, 96, 103, 96);
+    ctx.fill();
+
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    ctx.arc(101, 102, 2, 0, Math.PI * 2, true);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(89, 102, 2, 0, Math.PI * 2, true);
     ctx.fill();
   
 
